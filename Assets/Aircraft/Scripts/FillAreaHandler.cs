40ed7d31816137c986af395491dac5aebc61b9c8
Cevher Tekdal <tekdalcevher21@gmail.com>
2022-12-03 20:06:23 +0300

    Fuel low,empty animation added

--- Assets/Aircraft/Scripts/FillAreaHandler.cs
+++ Assets/Aircraft/Scripts/FillAreaHandler.cs
@@ -9,15 +9,25 @@ public class FillAreaHandler : MonoBehaviour
     [SerializeField] private Slider fuelBar;
     [SerializeField] private Sprite greenBar, orangeBar, redBar;
 
-    void Update(){
-        if(fuelBar.value >= 0.7f){
+    void Update()
+    {
+        if (fuelBar.value >= 0.7f)
+        {
             GetComponent<Image>().sprite = greenBar;
         }
-        else if(fuelBar.value >= 0.35 && fuelBar.value < 0.7f){
+        else if (fuelBar.value >= 0.35 && fuelBar.value < 0.7f)
+        {
             GetComponent<Image>().sprite = orangeBar;
         }
-        else if(fuelBar.value < 0.35f){
+        else if (fuelBar.value > 0f && fuelBar.value < 0.35f)
+        {
             GetComponent<Image>().sprite = redBar;
+            fuelBar.GetComponent<Animator>().Play("FuelLow_UI");
+        }
+        else if (fuelBar.value <= 0f)
+        {
+            GetComponent<Image>().sprite = redBar;
+            fuelBar.GetComponent<Animator>().Play("FuelEmpty_UI");
         }
     }
 }
